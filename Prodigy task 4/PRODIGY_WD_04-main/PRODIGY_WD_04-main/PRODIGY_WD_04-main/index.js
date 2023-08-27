38b67473e4480ab8e06450f25c6baf5c6e9e7043
Kritagy Shrivastava <142904074+kritagyshrivastava2002@users.noreply.github.com>
2023-08-27 15:34:47 +0530

    Add files via upload

---
+++ Prodigy task 4/PRODIGY_WD_04-main/PRODIGY_WD_04-main/PRODIGY_WD_04-main/index.js
@@ -0,0 +1,18 @@
+
+// window.scrollTo(0,300);
+// const element = document.getElementById('checkabout');
+// element.addEventListener("click", myFunction);
+
+// function myFunction() {
+//   setTimeout(() => {
+//     window.scrollTo(0,450);
+    
+//   }, 500);
+// }
+
+// typed js
+var typed = new Typed('#element', {
+    strings: ['Web Developer.', 'Youtuber.','Web Developer.'],
+    typeSpeed: 80,
+  });
+
